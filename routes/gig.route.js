c3b7ddd5a3eddb1474498bb6146492b1204f801d
VenkateshSircilla123 <sircillavenkatesh0@gmail.com>
2023-10-16 16:10:06 +0530

    update

---
+++ routes/gig.route.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import {  createGig, deleteGig, getGig, getGigs} from '../controllers/gig.controller.js'
+import { verifyToken } from "../middleware/jwt.js";
+
+
+const router = Router()
+
+router.get('/',getGigs)
+router.get('/single/:id', getGig)
+router.post('/',verifyToken,createGig)
+router.delete('/delete/:id',verifyToken,deleteGig)
+
+export default router
