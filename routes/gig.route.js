43fc6c6fe3b76999d7f3a9fc3951f6e003e46dd8
VenkateshSircilla123 <113850972+VenkateshSircilla123@users.noreply.github.com>
2023-10-17 22:13:25 +0530

    Update gig.route.js

--- routes/gig.route.js
+++ routes/gig.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import {  createGig, deleteGig, getGig, getGigs} from '../controllers/gig.controller.js'
+import {  createGig, deleteGig, getGig, getGigs, test} from '../controllers/gig.controller.js'
 import { verifyToken } from "../middleware/jwt.js";
 
 
 const router = Router()
 
 router.get('/',getGigs)
+router.get('/test',test)
 router.get('/single/:id', getGig)
 router.post('/',verifyToken,createGig)
 router.delete('/delete/:id',verifyToken,deleteGig)
